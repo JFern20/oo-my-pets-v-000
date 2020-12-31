eaf6e87baa74dead846ea6919e11ee660812bc49
Jonathan Fernandez <jfern20@gmail.com>
2020-12-31 00:35:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,6 @@
 class Cat
     attr_reader :name  #can't change its name   #getter 
-    attr_accessor :mood #can change its mood 
+    attr_accessor :mood #can change its mood    
    
    def initialize(name, mood = "nervous" )  #initializes with a nervous mood
      @name = name 
